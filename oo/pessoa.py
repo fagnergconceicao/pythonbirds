b22847519a3f7e296ea82380431b72116cd5b87e
Fagner <fagner00@gmail.com>
2020-08-27 23:14:31 -0300

    Criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,27 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=31):
+    def __init__(self, *filhos, nome=None, idade=31):
         self.idade = idade
         self.nome = nome
+        self.filhos = list (filhos)
 
 
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Fagner')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Gonzaga'
-    print(p.nome)
-    print(p.idade)
+    Márjorie = Pessoa (nome='Márjorie', idade=2)
+    Adriele = Pessoa (nome='Adriele', idade=0)
+    Fagner = Pessoa(Márjorie, Adriele,  nome='Fagner')
+    print(Pessoa.cumprimentar(Fagner))
+    print(Pessoa.cumprimentar(Márjorie))
+    print(Pessoa.cumprimentar(Adriele))
+    print(id(Fagner))
+    print(id(Márjorie))
+    print(id(Adriele))
+    print(Fagner.cumprimentar())
+    print(Fagner.nome)
+    print(Fagner.idade)
+    for filho in Fagner.filhos:
+        print(filho.nome, filho.idade)
+
 
